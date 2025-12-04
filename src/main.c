bd0550575a63bb47c43f32802e5dc87938cd9318
TerenceBeauregard <125586842+TerenceBeauregard@users.noreply.github.com>
2025-12-04 09:31:45 +0100

    Fix _car function call to use one argument

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
 		printf("%lf",r);
 	}
 	else if (strcmp(op, "car") == 0) {
-		r = _car(atof(a), atof(b));
+		r = _car(atof(a));
 		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
